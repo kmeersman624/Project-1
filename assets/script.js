9633441e7d024581de5a81523b843eada37baecf
kmeersman <katie4682@yahoo.com>
2020-05-19 14:51:06 -0600

    Built OMDB API AJAX call for random movie

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var movieAPI = "cac7fedb";
+var movieQueryURL = "http://www.omdbapi.com/?apikey=" + movieAPI + "&t=";
+console.log(movieQueryURL);
+// var posterQueryURL = http://img.omdbapi.com/?apikey=[yourkey]&
+var movieChoices = [
+  "Pulp Fiction",
+  "A Knight's Tale",
+  "Independence Day",
+  "Interstellar",
+  "The Princess Bride",
+  "Superbad",
+  "Star Wars: Episode V - The Empire Strikes Back",
+  "Silver Linings Playbook",
+  "Almost Famous",
+  "The Wrong Missy",
+  "Date Night",
+];
+
+let randomIndex = Math.floor(Math.random() * movieChoices.length);
+
+$.ajax({
+  url: (movieQueryURL += movieChoices[randomIndex]),
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
