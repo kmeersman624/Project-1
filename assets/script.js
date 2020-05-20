8249a76cd1d1a46589582aa75e1166bafb025653
kmeersman <katie4682@yahoo.com>
2020-05-20 15:24:52 -0600

    worked on apis and functions

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,34 @@
-var mealAPI = "1"
-
-var mealQueryURL= "https://www.themealdb.com/api/json/v1/1/random.php" + mealQueryURL + "&random";
-
+var mealAPI = "1";
+var mealQueryURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 $.ajax({
-    url: mealqueryURL,
-    method: "GET"
-}).then(function(response){
-console.log(response);
-})
+  url: mealQueryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+});
 
+//***************************************************** */
 var drinkAPI = "1";
 var drinkQueryUrl = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-$.ajax({
+function displayDrinkInfo() {
+  $.ajax({
     url: drinkQueryUrl,
-    method: "GET"
-}).then(function(response){
+    method: "GET",
+  }).then(function (response) {
     console.log(response);
-})
-
+    // var drinkImg = response.
+    // var drinkTitle = response.
+    // var drinkRecipe = response.
+    const drinkTitle = result.meals[0];
+    for(let i = 1; i < 21; i++){
+      if(meal["strIngredient" + i]){
+        console.log(meal["strIngredient" + i]);
+        console.log(meal["strMeasure" + i]);
+  });
+}
+//********************************************************************** */
 var movieAPI = "cac7fedb";
 var movieQueryURL = "http://www.omdbapi.com/?apikey=" + movieAPI + "&t=";
 console.log(movieQueryURL);
@@ -40,9 +49,18 @@ var movieChoices = [
 
 let randomIndex = Math.floor(Math.random() * movieChoices.length);
 
-$.ajax({
-  url: (movieQueryURL += movieChoices[randomIndex]),
-  method: "GET",
-}).then(function (response) {
-  console.log(response);
-});
+function displayMovieInfo() {
+  $.ajax({
+    url: (movieQueryURL += movieChoices[randomIndex]),
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    var poster = response.Poster;
+    var movieTitle = response.Title;
+    var plot = response.Plot;
+    $("#movieImg").append(poster);
+    $("#movieTitle").append(movieTitle);
+    $("#plot").append(plot);
+  });
+}
+$(document).on("click", "#generate", displayMovieInfo);
